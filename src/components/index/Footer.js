3b667b9916f01eb5238eb0c008587f0fbd546b2f
Fern knee <kevinjbritten@gmail.com>
2021-08-18 12:17:54 -0400

    added footer component

---
+++ src/components/index/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h3>nuSite</h3>
+      <div>
+        <p>info@nusite.com</p>
+        <p>Montreal, Qc</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
