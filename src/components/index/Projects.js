ada789838b0e3f3379c60ceca8e187a4e9958132
Fern knee <kevinjbritten@gmail.com>
2021-08-19 13:21:28 -0400

    added project svgs

--- src/components/index/Projects.js
+++ src/components/index/Projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { StaticImage } from "gatsby-plugin-image";
+
 function Projects() {
   return (
     <section id="projects">
@@ -16,29 +18,42 @@ function Projects() {
             <div>
               <h3>Handford Creative &#38; Imaging</h3>
               <p>HTML / CSS / JavaScript / WebFlow / CMS</p>
+
               <a href="https://www.handford.com">View Live Site</a>
             </div>
-            <h1>01</h1>
+            <StaticImage
+              objectFit="contain"
+              src="../../images/handford-project.svg"
+              alt=""
+            />
           </div>
         </div>
         <div class="project__item-outer">
           <div className="project__item">
             <div>
-              <h3>Handford Creative &#38; Imaging</h3>
-              <p>HTML / CSS / JavaScript / WebFlow / CMS</p>
+              <h3>Marie Khouri Sculpture</h3>
+              <p>HTML / CSS / JavaScript / Vue.Js / CMS</p>
               <a href="https://www.handford.com">View Live Site</a>
             </div>
-            <h1>01</h1>
+            <StaticImage
+              objectFit="contain"
+              src="../../images/marie-khouri-project.svg"
+              alt=""
+            />{" "}
           </div>
         </div>
         <div class="project__item-outer">
           <div className="project__item">
             <div>
-              <h3>Handford Creative &#38; Imaging</h3>
-              <p>HTML / CSS / JavaScript / WebFlow / CMS</p>
+              <h3>Bradbury Architecture</h3>
+              <p>HTML / CSS / JavaScript / Vue.Js / CMS</p>
               <a href="https://www.handford.com">View Live Site</a>
             </div>
-            <h1>01</h1>
+            <StaticImage
+              objectFit="contain"
+              src="../../images/bradbury-project.svg"
+              alt=""
+            />{" "}
           </div>
         </div>
       </div>
